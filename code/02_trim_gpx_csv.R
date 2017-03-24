51ec232c0322753d63d3388031c752c00ca88959
Michelle Stuart <bunx21@yahoo.com>
2017-03-24 14:17:09 -0400

    changes

--- code/02_trim_gpx_csv.R
+++ code/02_trim_gpx_csv.R
@@ -25,72 +25,77 @@ surv <- surv %>%
   separate(PauseEnd, into = c("baddate4", "pausend"), sep = " ") %>% 
   select(-contains("bad")) 
 
-# combine date and time to form a dttm column
+# combine date and time to form a dttm column ####
   # set time zone to PHT, Asia/Manila
-surv$start <- stringr::str_c(surv$Date, surv$start, sep = " ")  
+surv$start <- str_c(surv$Date, surv$start, sep = " ")  
 surv$start <- ymd_hms(surv$start)
 surv$start <- force_tz(surv$start, tzone = "Asia/Manila")
-surv$end <- stringr::str_c(surv$Date, surv$end, sep = " ")  
+surv$end <- str_c(surv$Date, surv$end, sep = " ")  
 surv$end <- ymd_hms(surv$end)
 surv$end <- force_tz(surv$end, tzone = "Asia/Manila")
-surv$paust <- stringr::str_c(surv$Date, surv$paust, sep = " ")  
+surv$paust <- str_c(surv$Date, surv$paust, sep = " ")  
 surv$paust <- ymd_hms(surv$paust)
 surv$paust <- force_tz(surv$paust, tzone = "Asia/Manila")
-surv$pausend <- stringr::str_c(surv$Date, surv$pausend, sep = " ")  
+surv$pausend <- str_c(surv$Date, surv$pausend, sep = " ")  
 surv$pausend <- ymd_hms(surv$pausend)
 surv$pausend <- force_tz(surv$pausend, tzone = "Asia/Manila")
 
-# change time zone to UTC
+# change time zone to UTC ####
 surv$start <- with_tz(surv$start, tzone = "UTC")
 surv$end <- with_tz(surv$end, tzone = "UTC")
 surv$paust <- with_tz(surv$paust, tzone = "UTC")
 surv$pausend <- with_tz(surv$pausend, tzone = "UTC")
 
-# Read in each GPX file
+# Read in each GPX file ####
 files  <-  list.files(path = "data/gpx/", pattern="*Track.*gpx")
 len = length(files)
 
 for(i in 1:len){ # for each file
 	infile <- readGPXGarmin(paste("data/gpx/", files[i], sep="")) 
-	# start time of the GPS track in POSIXlt format
-	intimes <- ymd_hms(as.character(infile$data$time), tzone = "UTC")
-	intimes <- intimes[!is.na(intimes)]
+	header <- infile$header
+	data <- infile$data
+	data$time <- ymd_hms(data$time)
+	data <- arrange(data, time)
 	# start time for this GPX track
-	instarttime <-  intimes[1] 
+	instarttime <-  data$time[1]
 	# end time for this GPX track
-	inendtime = intimes[length(intimes)] 
+	inendtime = data$time[nrow(data)]
+	# change elevation to zero
+	data$elev <- 0
 	
-	# which survey started after the gpx and ended before the gpx
+	# which survey started after the gpx and ended before the gpx ####
 	inds <- surv %>% 
 	  filter(start >= instarttime & end <= inendtime)
-	# if none of the surveys fit
+	
+	# if none of the surveys fit ####
 	if(nrow(inds) == 0){
-		print(paste('File', files[i], 'does not cover a complete survey'))
+		print(str_c("File", files[i], "does not cover a complete survey"))
+	  
 	  # find a survey that at least starts or ends within this GPX track
 	  # which survey ended before the gpx and ended after the gpx started or started before the gpx ended and started after the gpx started
-		inds = which((surv$end <= inendtime & surv$end > instarttime) | (surv$start < inendtime & surv$start >= instarttime)) 
+		inds <- surv %>% 
+		  filter((end <= inendtime & end >= instarttime)| (start <= inendtime & start >= instarttime))
 		if(length(inds) == 0){
-			print(paste('EVEN WORSE:', files[i], 'does not cover even PART of a survey'))
+			print(str_c("EVEN WORSE:", files[i], "does not cover even PART of a survey"))
 		}
 	}
-	if(length(inds) > 0){
+	if(nrow(inds) > 0){
 		for(j in inds){ # step through each survey that fits within this track (one or more)
 			# output all: not just if this was a dive for collecting APCL
 		  # if no pause
 		  if(is.na(surv$paust[j])){ 
 		    # find the GPX points that fit within the survey
-				k <- which(intimes >= surv$start[j] & intimes <= surv$end[j]) 
-				outfile <- list(header = infile$header, data = infile$data[k,])
-				outfile$data$elev = 0 # set elevation to 0
-				writeGPX(filename = paste("data/gpx_trimmed/", surv$DiveNum[j], '_', files[i], sep=''), outfile = outfile)
+				k <- data %>% 
+				  filter(time >= surv$start[j] & time <= surv$end[j]) 
+				outfile <- list(header, k)
+				
+				writeGPX(filename = paste("data/gpx_trimmed/", surv$DiveNum[j], "_", files[i], sep=""), outfile = outfile)
 			}
 			if(!is.na(surv$paust[j])){ # account for a pause if need be
 				k1 = which(intimes >= surv$start[j] & intimes <= surv$paust[j]) # find the GPX points during survey and before pause
 				k2 = which(intimes >= surv$pausend[j] & intimes <= [j]) # find the GPX points after pause and during survey
 				outfile1 = list(header = infile$header, data = infile$data[k1,])
 				outfile2 = list(header = infile$header, data = infile$data[k2,])
-				outfile1$data$elev = 0 # set elevation to 0
-				outfile2$data$elev = 0 # set elevation to 0
 
 				nm = unlist(strsplit(files[i], '.gpx'))
 				writeGPX(filename = paste('data/gpx_trimmed/', surv$DiveNum[j], '_', nm, '_1.gpx', sep=''), outfile = outfile1) # write as two tracks
--- code/writeGPXGarmin.R
+++ code/writeGPXGarmin.R
@@ -11,18 +11,19 @@
 
 writeGPX = function(outfile, filename){
 	con = file(filename, open = 'wt')
-	cat(outfile$header, file=con) # write the same header
+	cat(header, file=con) # write the same header
 
-	# write each trkpt
-	for(i in 1:nrow(outfile$data)){
+	# add the data
+	
+	for(i in 1:nrow(outfile)){
 		cat('<trkpt lat="', file=con)
-		cat(outfile$data$lat[i], file=con)
+		cat(outfile$lat[i], file=con)
 		cat('" lon="', file=con)
-		cat(outfile$data$lon[i], file=con)
+		cat(outfile$lon[i], file=con)
 		cat('"><ele>', file=con)
-		cat(outfile$data$elev[i], file=con)
+		cat(outfile$elev[i], file=con)
 		cat('</ele><time>', file=con)
-		cat(as.character(outfile$data$time[i]), file=con)
+		cat(as.character(outfile$time[i]), file=con)
 		cat('</time></trkpt>', file=con)
 	}
 
